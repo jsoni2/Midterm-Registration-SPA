f9a6d4abbfd77d3286a703a96451e3726be39bb0
Janak Soni <jakzi7005@gmail.com>
2017-03-28 02:44:15 -0700

    Done adding suffix

--- js/app.js
+++ js/app.js
@@ -6,63 +6,92 @@ const myApp = angular.module('myApp', ['ngRoute']).config([
 ]).config([
   '$routeProvider',
   function($routeProvider) {
-
     $routeProvider.when('/', {templateUrl: 'views/home.html'}).when('/signup', {templateUrl: 'views/signup.html'}).otherwise({redirectTo: '/'})
   }
 ])
 
 myApp.controller("moduleController", function($scope) {
   $scope.modules = []
-  $scope.showModuleForm = false
-  $scope.showSignUpForm = false
+  $scope.showModuleForm = true
+  $scope.showSignUpForm = true
 
   $scope.addModule = function() {
     $scope.modules.push({"name": $scope.modulename.toUpperCase(), "website": $scope.modulewebsite})
     $scope.modulename = ""
     $scope.modulewebsite = ""
+    /* Code modified from function suffixDuplicates obtained from http://stackoverflow.com/questions/10643933/renaming-duplicates-in-javascript-array */
+    let count = { }
+    let firstOccurences = { }
+    let item, itemCount
+    for (let i = 0; i < $scope.modules.length; i++) {
+      if ($scope.modules[i].name.indexOf(' ') >= 0) {
+        $scope.modules[i].name=$scope.modules[i].name.substr(0, $scope.modules[i].name.indexOf(" "))
+      }
+      item = $scope.modules[i].name
+      itemCount = count[ item ]
+      itemCount = count[ item ] = ( itemCount == null ? 1 : itemCount + 1 )
+      if (itemCount == 2) {
+        $scope.modules[ firstOccurences[item]].name = $scope.modules[firstOccurences[item]].name + " - Group 1"
+      }
+      if (count[item] > 1) {
+        $scope.modules[i].name = $scope.modules[i].name + " - Group " + count[item]
+      }
+      else {
+        firstOccurences[item] = i
+      }
+    }
   }
 
   $scope.addMember = function() {
     for (module of $scope.modules) {
-      console.log(module);
-      console.log($scope.selectedmodule);
       if (module.name === $scope.selectedmodule.name) {
         if (!module.members) {
-          console.log('or here?');
           module.members=[]
           module.members.push({"firstname": $scope.firstname, "lastname": $scope.lastname, "emailaddress": $scope.emailaddress})
         }
         else {
-          console.log('here?');
           module.members.push({"firstname": $scope.firstname, "lastname": $scope.lastname, "emailaddress": $scope.emailaddress})
         }
-        console.log('going in if');
         $scope.firstname = ""
         $scope.lastname = ""
         $scope.emailaddress = ""
         $scope.selectedmodule = ""
-        console.log(module.members);
-      }
-      else {
-        console.log('not going in loop');
       }
     }
-    console.log(JSON.stringify($scope.modules, null, "\t"));
   }
 
   $scope.removeModule = function(module) {
     let index = $scope.modules.indexOf(module)
     $scope.modules.splice(index, 1)
+    /* Code modified from function suffixDuplicates obtained from http://stackoverflow.com/questions/10643933/renaming-duplicates-in-javascript-array */
+    let count = { }
+    let firstOccurences = { }
+    let item, itemCount
+    for (let i = 0; i < $scope.modules.length; i++) {
+      if ($scope.modules[i].name.indexOf(' ') >= 0) {
+        $scope.modules[i].name=$scope.modules[i].name.substr(0, $scope.modules[i].name.indexOf(" "))
+      }
+      item = $scope.modules[i].name
+      itemCount = count[ item ]
+      itemCount = count[ item ] = ( itemCount == null ? 1 : itemCount + 1 )
+      if (itemCount == 2) {
+        $scope.modules[ firstOccurences[item]].name = $scope.modules[firstOccurences[item]].name + " - Group 1"
+      }
+      if (count[item] > 1) {
+        $scope.modules[i].name = $scope.modules[i].name + " - Group " + count[item]
+      }
+      else {
+        firstOccurences[item] = i
+      }
+    }
   }
 
   $scope.removeMember = function(module,member) {
-    //let index = $scope.modules.indexOf(module)
     for (mod of $scope.modules) {
       if (mod===module) {
         let index = mod.members.indexOf(member)
         mod.members.splice(index, 1)
       }
     }
-    console.log(JSON.stringify($scope.modules, null, "\t"));
   }
 })
