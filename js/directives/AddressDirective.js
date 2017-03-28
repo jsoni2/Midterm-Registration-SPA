ae9b27922cc96a17268d2d9bf64e7130218ec370
Janak Soni <jakzi7005@gmail.com>
2017-03-28 05:20:37 -0700

    Deleted unnecessary files

--- js/directives/AddressDirective.js
+++
@@ -1,9 +0,0 @@
-myApp.directive('myAddress', function() {
-    return {
-        route: {
-            data: '='
-        },
-        restrict: 'E',
-        templateUrl: 'views/directives/my-address.html'
-    }
-})
